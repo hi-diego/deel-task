563db567b07516807b7835a71293c335a85e8d65
DiegoMeza <hi@diego.pro>
2022-12-07 16:10:42 -0600

    contract/:id  fixed and /contracts endpoint added

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {Op} = require('sequelize')
 const {sequelize} = require('./model')
 const {getProfile} = require('./middleware/getProfile')
 const app = express();
@@ -16,6 +17,21 @@ app.get('/contracts/:id', getProfile ,async (req, res) =>{
     const {id} = req.params
     const contract = await Contract.findOne({where: {id}})
     if(!contract) return res.status(404).end()
+    let profileId = Number(req.get('profile_id') || '0');
+    if(contract.ContractorId !== profileId) return res.status(401).end()
     res.json(contract)
+})
+
+app.get('/contracts', getProfile ,async (req, res) =>{
+    const {Contract} = req.app.get('models')
+    let profileId = Number(req.get('profile_id') || '0');
+    const contracts = await Contract.findAll({
+      where: {
+        ContractorId: {
+          [Op.eq]: profileId
+        }
+      }
+    });
+    res.json(contracts)
 })
 module.exports = app;
